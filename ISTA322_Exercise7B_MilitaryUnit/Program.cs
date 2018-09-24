7a41d776528536bd6e5b51b9223fd391f01bbe4e
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-23 22:42:01 -0400

    Add project files.

---
+++ ISTA322_Exercise7B_MilitaryUnit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTA322_Exercise7B_MilitaryUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
